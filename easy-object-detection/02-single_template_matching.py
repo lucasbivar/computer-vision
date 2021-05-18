49dbe2b9ddb8101b01188c037561030bf3f646fa
lucasbivar <lucasbivarfonseca@gmail.com>
2021-05-17 23:22:30 -0300

    adding single template matching

---
+++ easy-object-detection/02-single_template_matching.py
@@ -0,0 +1,55 @@
+# USAGE
+# python 02-single_template_matching.py --image images/coke_bottle.png --template images/coke_logo.png
+# python 02-single_template_matching.py --image images/8_diamonds.png --template images/diamonds_template.png
+
+# in template matching the orientation of the template and the image must be the same
+# because the algorithm just sweep pixel by pixel
+# Furthermore, the scale must be the same too
+
+# import the necessary pages
+import argparse
+import cv2
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", type=str, required=True,
+	help="path to input image where we'll apply template matching")
+ap.add_argument("-t", "--template", type=str, required=True,
+	help="path to template image")
+args = vars(ap.parse_args())
+
+# load the input image and template image from disk, then display
+# them to  our screen
+print("[INFO] loading images...")
+image = cv2.imread(args["image"])
+template = cv2.imread(args["template"])
+cv2.imshow("Image", image)
+cv2.imshow("Template", template)
+
+# convert both the image and template to grayscale
+imageGray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+templateGray = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+
+# perform template matching
+print("[INFO] performing template matching...")
+result = cv2.matchTemplate(imageGray, templateGray,
+	cv2.TM_CCOEFF_NORMED)
+cv2.imshow("Template Matching Result", result)	
+
+# find the min location and the max location values and positions
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(result)
+
+# if you want filter the errors (false positives)
+if maxVal > 0.9:
+	# determine the starting and ending (x, y)-coordinates of the
+	# bounding box
+	(startX, startY) = maxLoc
+	endX = startX + template.shape[1]
+	endY = startY + template.shape[0]
+
+	# draw the bounding box on the image
+	cv2.rectangle(image, (startX, startY), (endX, endY), (255, 0, 0), 3)
+
+# show the output image
+cv2.imshow("Output", image)
+cv2.waitKey(0)
